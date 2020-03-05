b60ce55782bd799100298446506de4c7bdb9c1a3
longxiumei <3276198935@qq.com>
2020-03-05 17:27:30 +0800

    dev的第一次提交

---
+++ src/cn/appsys/pojo/Test.java
@@ -0,0 +1,13 @@
+package cn.appsys.pojo;
+
+public class Test {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
